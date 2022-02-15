3c7c817fc4632e2bba6ba13fb6b8fdf393717e3b
IrynaAn <airyna1@mail.ru>
2022-02-15 11:12:47 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a=str.split("");
+
+  for (let i=0; i<bracketsConfig.length; i++) {
+    let b=bracketsConfig[i];
+    for (let k=0; k<a.length; k++) {
+      if (a[k]===b[0]&&a[k+1]===b[1]){
+      a.splice(k,2);k--;i=-1;
+      if (!a.length){return true}
+    }}}
+
+return false;
 }
